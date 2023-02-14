b9435e4c640770ebb910122c3f011dee43a05a05
stella <kirtimukha@nate.com>
2023-02-14 19:36:06 +0900

    [U] Router.tsx 설정

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,8 +1,21 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
+import Coin from "./routes/Coin";
+import Coins from "./routes/Coins";
 
 function Router() {
-	return <>test</>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/:coinId">
+					<Coin />
+				</Route>
+				<Route path="/">
+					<Coins />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default Router;
